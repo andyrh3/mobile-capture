033b0073bde3019c0d3c744668d3fbc84bee55c2
Andrew Harrison <andyrh3@gmail.com>
2019-02-18 13:04:01 +0000

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+import '../scss/styles.scss';
+import CondorLogo from '../images/condor-logo.min.svg';
+import TcaHoriLogo from '../images/tca-logo-hori.min.svg';
+import TcaVertLogo from '../images/tca-logo-vert-neg.min.svg';
+
+/*function component() {
+  let element = document.createElement('div');
+  let btn = document.createElement('button');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('test');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
+  // Add the image to our existing div.
+  var condorLogo = new Image();
+  condorLogo.src = CondorLogo;
+  element.appendChild(condorLogo);
+
+  return element;
+}
+
+document.body.appendChild(component());*/
+
+document.getElementsByClassName('li.country');
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+/*
+const WriteFileWebpackPlugin = require('./node_modules/write-file-webpack-plugin/dist/WriteFileWebpackPlugin.js');
+*/
+
+module.exports = {
+    mode: "development",
+    entry: [
+        './src/js/index.js',
+        './src/scss/styles.scss'
+    ],
+    output: {
+        filename: './js/bundle.min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|gif|jpeg|jpg|svg)/,
+                exclude:[/fonts/],
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: './images/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                exclude:[/images/],
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+/*                        name: '[name].[ext]',
+                        outputPath: '/fonts/'*/
+                        name: '[name].[ext]',
+                        mimetype: 'application/font-woff',
+                        publicPath: '/fonts',
+                        outputPath: './fonts/'
+                    }
+                }]
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
+            }
+        ]
+    },
+    devServer: {
+        publicPath: "/",
+        contentBase: 'dist',
+        host: 'localhost',
+        port: 9001
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin('./css/style.min.css')
+    ],
+
+
+};
+
+
